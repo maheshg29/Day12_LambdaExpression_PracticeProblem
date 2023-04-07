be07f4f82fe858a08a84fd7cae5d7d72c6446de2
mahesh <gangurdemahesh9@gmail.com>
2023-04-07 18:50:48 +0530

    [mahesh]add program file for Welcome greeting

---
+++ Day12_LambdaExpression_PracticeProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_LambdaExpression_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
